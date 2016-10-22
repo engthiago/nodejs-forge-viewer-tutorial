373fd68e404a9347414c9f3d7dfa42654e015261
engthiago <t.almeidaeng@gmail.com>
2016-10-22 06:14:47 -0300

    test1

--- www/js/index.js
+++ www/js/index.js
@@ -16,4 +16,52 @@
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////////////////
 
-// See Readme file for JS code to insert here.
+var defaultUrn = '<dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE2LTEwLTIyLTA5LTA3LTA1LXFoMmxremJteXkyMjh0Z25ydWJ4bjJoenowaGcvaW50cm8tdmlld2VyLnJ2dA>';
+var savedToken = {};
+var tokenurl = window.location.protocol + '//' + window.location.host + '/api/token';
+var notificationChannel = '';
+
+$(document).ready(function () {
+
+    var config = {
+        environment : 'AutodeskProduction'
+    };
+
+    // Instantiate viewer factory
+    var viewerFactory = new Autodesk.ADN.Toolkit.Viewer.AdnViewerFactory(
+        tokenurl,
+        config);
+
+    // Allows different urn to be passed as url parameter
+    var paramUrn = Autodesk.Viewing.Private.getParameterByName('urn');
+    var urn = (paramUrn !== '' ? paramUrn : defaultUrn);
+
+    viewerFactory.getViewablePath (urn,
+        function(pathInfoCollection) {
+            var viewerConfig = {
+                viewerType: 'GuiViewer3D'
+                // viewerType: 'Viewer3D' // If a viewer without a toolbar is wanted
+            };
+            var viewer = viewerFactory.createViewer(
+                $('#viewerDiv')[0],
+                viewerConfig);
+            viewer.load(pathInfoCollection.path3d[0].path);
+        }, onError);
+
+});
+
+// I use this call if you want to get back an object json of your token
+
+var tokenAjax = function(handleData) {
+      $.ajax({
+        url:tokenurl,
+        dataType: 'json',  
+        success:function(data) {
+          handleData(data); 
+        }
+    });
+}
+
+function onError(error) {
+    console.log('Error: ' + error);
+};
